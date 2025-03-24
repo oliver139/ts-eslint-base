b47519c38223132a508bbdd6af7cae9be0fce8c3
Oliver Mak <oliver139.working@gmail.com>
2025-03-24 17:41:33 +0800

    Lint and update dependencies

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,42 +1,42 @@
-import json from "@rollup/plugin-json";
-import del from "rollup-plugin-delete";
-import { dts } from "rollup-plugin-dts";
-import esbuild from "rollup-plugin-esbuild";
+import json from '@rollup/plugin-json'
+import del from 'rollup-plugin-delete'
+import { dts } from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        dir: "dist/cjs",
-        format: "cjs",
+        dir: 'dist/cjs',
+        format: 'cjs',
         sourcemap: false,
       },
       {
-        dir: "dist/esm",
-        format: "esm",
+        dir: 'dist/esm',
+        format: 'esm',
         sourcemap: false,
       },
     ],
     plugins: [
-      del({ targets: "dist/*" }),
+      del({ targets: 'dist/*' }),
       esbuild({
         minify: true,
-        drop: ["console", "debugger"],
+        drop: ['console', 'debugger'],
       }),
       json(),
     ],
   },
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        file: "dist/cjs/index.d.cts",
-        format: "cjs",
+        file: 'dist/cjs/index.d.cts',
+        format: 'cjs',
       },
       {
-        file: "dist/esm/index.d.ts",
-        format: "esm",
+        file: 'dist/esm/index.d.ts',
+        format: 'esm',
       },
     ],
     plugins: [dts({
@@ -45,4 +45,4 @@ export default [
       respectExternal: true,
     })],
   },
-];
+]
